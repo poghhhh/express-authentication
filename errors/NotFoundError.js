02f5b146483e6bb2074cb8336bd4f9a2d3420a8d
THANHPHONGVO\vthan <THANHPHONGVO>
2024-03-06 01:17:25 +0700

    commit

--- errors/NotFoundError.js
+++ errors/NotFoundError.js
@@ -1,8 +1,8 @@
 class NotFoundError extends Error {
-    constructor(message) {
-        super(message);
-        this.status = 404;
-    }
+  constructor(message) {
+    super(message);
+    this.status = 404;
+  }
 }
 
 module.exports = NotFoundError;
--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -1,8 +1,8 @@
 class UnauthorizedError extends Error {
-    constructor(message) {
-        super(message);
-        this.status = 401;
-    }
+  constructor(message) {
+    super(message);
+    this.status = 401;
+  }
 }
 
 module.exports = UnauthorizedError;
